e07544c988ee206b61bf70626b9fb32207bca630
Jairo Andres Wilches <andresing.81.17@gmail.com>
2021-12-05 21:44:04 -0500

    Se actualiza la url de mongo

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ import { config } from "dotenv";
 config();
 
 export default {
-  mongodbURL: process.env.MONGODB_URI || "mongodb://Localhost/tasksdb",
-};
+  // mongodbURL: process.env.MONGODB_URI || "mongodb://Localhost/tasksdb",
+  mongodbURL: "mongodb+srv://geouser:E438HNlXKIg9RbYE@cluster0.m9kbz.mongodb.net/geobikedb?retryWrites=true&w=majority"
+}
